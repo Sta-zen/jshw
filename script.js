08972ddd6b503e1fe85a0225f06dc9d3532d1865
Roma <romchik4353@gmail.com>
2020-01-20 18:52:55 +0500

    jshw

---
+++ script.js
@@ -0,0 +1,16 @@
+var name = prompt('Введите своё имя');
+console.log( 'Имя пользователя - ' + name);
+var age = +prompt('Ваш возрост')
+console.log( 'Возрост пользователя - ' + age);
+var ex1 = prompt('20+10=?')
+console.log('20+10=30 (Ваш ответ - ' + ex1 +')');
+var ex2 = prompt('17-9=?')
+console.log('17-9=8 (Ваш ответ - ' + ex2 +')');
+var ex3 = prompt('28/4=?')
+console.log('28/4=7 (Ваш ответ - ' + ex3 +')');
+var ex4 = prompt('8*7=?')
+console.log('8*7=56 (Ваш ответ - ' + ex4 +')');
+var ex4 = prompt('30%4=?')
+console.log('30%4=2 (Ваш ответ - ' + ex4 +')');
+var con = ('Перейдите в консоль')
+alert (con)``
